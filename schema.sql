2aeb44c3b9bb094b536aece470ad72d208c602a6
Konstantinos Agiannis <k.agiannis@yandex.com>
2019-06-04 11:02:42 +0300

    schema in seperate file

---
+++ schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS words(
+	form TEXT,
+	lemma TEXT,
+	pos TEXT,
+	greek_pos TEXT,
+	gender TEXT,
+	ptosi TEXT,
+	number TEXT,
+	person NUM,
+	tense TEXT,
+	aspect TEXT,
+	mood TEXT,
+	verbform TEXT,
+	voice TEXT,
+	definite TEXT,
+	degree TEXT,
+	prontype TEXT,
+	poss TEXT,
+	tags TEXT,
+	freq INT
+);
+
+CREATE TABLE IF NOT EXISTS def(
+	lemma TEXT,
+	def TEXT
+);
+
+CREATE TABLE IF NOT EXISTS synonyms(
+	lemma TEXT,
+	syn TEXT
+);
+
+CREATE TABLE IF NOT EXISTS related(
+	lemma TEXT,
+	rel TEXT
+);
+
+CREATE TABLE IF NOT EXISTS etymology(
+	lemma TEXT,
+	rel TEXT
+);
