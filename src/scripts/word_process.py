40edb782ced1800818da80df721d8f26897b2edc
Konstantinos Agiannis <konstantinos@debian.Home>
2019-07-25 17:02:06 +0300

    add more cases to word process

--- src/scripts/word_process.py
+++ src/scripts/word_process.py
@@ -9,16 +9,20 @@ parser = argparse.ArgumentParser(description='''
 
 optional = parser.add_argument_group('optional arguments')
 
-optional.add_argument('--min-count', help='minimun number of occurances (default = 1)',
-                      dest='min_count', type=int, default=1)
+optional.add_argument('--min-freq', help='minimun number of occurances (default = 1)',
+                      dest='min_freq', type=int, default=1)
+
 optional.add_argument('--no-print-freq', help="don't print fequency count",
                       dest='no_print_freq',action='store_true')
+
+optional.add_argument('--min-capital-freq',help='''minimun number of occurances 
+       for words containing only capital letters (default = 1)''',dest='min_cap_freq',type=int,default = 1)
 
 args = parser.parse_args()
 
 words = {}
 
-min_freq_count = args.min_count
+min_freq_count = args.min_freq
 
 in_file = sys.stdin
 line = in_file.readline()
@@ -55,9 +59,15 @@ for x,y in words.items():
 	
 	if y < min_freq_count:
 		continue
-	
-	elif re.fullmatch("[αβγδεζηθικλμνξοπρστυφχψωςϋϊ]*[αεηιυοωϋϊ]+[βγδζθκλμνξπρστφχψς]+[αεηιυοωϋϊ]+[αβγδεζηθικλμνξοπρστυφχψωςϋϊ]*",x,re.UNICODE) is not None:
+	# remove words with no accent and at least 2 sylables
+	elif re.fullmatch(r"[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΫΪαβγδεζηθικλμνξοπρστυφχψωςϋϊ]*[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΫΪαεηιυοωϋϊ]+[βγδζθκλμνξπρστφχψς]+[αεηιυοωϋϊ]+[αβγδεζηθικλμνξοπρστυφχψωςϋϊ]*",x,re.UNICODE) is not None:
 		# print('#άτονο#',x)
+		continue
+	elif re.
+	# remove words with probably wrong capital letters (at least 2 in the beginning) or with non capital in the middle
+	elif re.search(r'([ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]{2,}[αβγδεζηθικλμνξοπρστυφχψωςάέήίόύώΐΰϋϊἱ]|[αβγδεζηθικλμνξοπρστυφχψωςάέήίόύώΐΰϋϊἱ][ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΆΈΉΊΌΎΏΫΪ])',x,re.UNICODE) is not None:
+		continue
+	elif re.fullmatch(r'[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΆΈΉΊΌΎΏΫΪ]+', x, re.UNICODE) is not None and y < args.min_cap_freq:
 		continue
 	# if there is the same word in lower case, skip it	
 	elif x != x.lower():
